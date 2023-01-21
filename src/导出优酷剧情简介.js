cc2841f5dc432d2bce85b460e059864effe5aca9
Karsten Zhou <1339688323@qq.com>
2023-01-21 09:50:42 +0800

    添加导出优酷剧情简介的相关代码

--- src/导出优酷剧情简介.js
+++ src/导出优酷剧情简介.js
@@ -12,10 +12,12 @@ const sleep = require('await-sleep');
         let res = await axios.get(`https://list.youku.com/show/point?id=${id}&stage=reload_${reload}&callback=a`);
         res = res['data'].slice(14, -2);
         res = JSON.parse(res);
-        console.log(res);
+        // console.log(res);
         if (res.error === 1) {
+            console.log(`导出完成，请查看文件：优酷剧情简介（id：${id}）.html`);
             break;
         }
+        console.log(`正在加载第${reload}~${reload + 9}集的剧情简介……`);
         reload = reload + 10;
         res = res['html'];
         fs.writeFileSync(path.join(__dirname, `优酷剧情简介（id：${id}）.html`), res, { flag: 'a' });
