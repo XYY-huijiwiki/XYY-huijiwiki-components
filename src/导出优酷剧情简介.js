b1056abca0f012776d7a1e89810714b479be50f5
Karsten Zhou <1339688323@qq.com>
2023-01-21 09:41:30 +0800

    Create 导出优酷剧情简介.js

---
+++ src/导出优酷剧情简介.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+const axios = require('axios');
+const sleep = require('await-sleep');
+
+(async (id) => {
+
+    let reload = 1;     //初始化reload
+    fs.writeFileSync(path.join(__dirname, `优酷剧情简介（id：${id}）.html`), '');   //初始化html
+
+    do {
+        let res = await axios.get(`https://list.youku.com/show/point?id=${id}&stage=reload_${reload}&callback=a`);
+        res = res['data'].slice(14, -2);
+        res = JSON.parse(res);
+        console.log(res);
+        if (res.error === 1) {
+            break;
+        }
+        reload = reload + 10;
+        res = res['html'];
+        fs.writeFileSync(path.join(__dirname, `优酷剧情简介（id：${id}）.html`), res, { flag: 'a' });
+        await sleep(1500);
+    } while (true);
+
+})(614193);
