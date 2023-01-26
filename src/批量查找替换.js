6340bb53dcedcaa409c9e14b52c6d1203037cc40
Karsten Zhou <1339688323@qq.com>
2023-01-26 21:56:29 +0800

    新增获取全部剧集信息的js

--- src/批量查找替换.js
+++ src/批量查找替换.js
@@ -8,6 +8,14 @@ let txt_list = fs.readdirSync(path.join(__dirname, 'wikitext'))
 txt_list.forEach(element => {
     console.log(element);
     let txt_content = fs.readFileSync(path.join(__dirname, 'wikitext', element), { encoding: 'utf8' })
-    txt_content = txt_content.replace(`[[分类:奇妙大营救]]`, `\n==参考资料==\n{{参考资料}}`)
+    txt_content = txt_content.replace(`[[分类:运动篇]]`, ``)
+    // txt_content = txt_content.replace(`{{智趣羊学堂}}`, `{{导航}}\n\n==参考资料==\n{{参考资料}}`)
+    txt_content = txt_content.replace(`{{智趣羊学堂}}`, `{{导航}}`)
+    txt_content = txt_content.replace(`{{智趣羊学堂|不折叠}}`, `{{导航}}`)
+    txt_content = txt_content.replace(/[\n\r]{3}/g, `\r\r`)
+    txt_content = txt_content.replace(/[\n\r]{3}/g, `\r\r`)
+    txt_content = txt_content.replace(/[\n\r]{3}/g, `\r\r`)
+    txt_content = txt_content.replace(/[\n\r]{3}/g, `\r\r`)
+    txt_content = txt_content.replace(/智趣羊学堂 /, `智趣羊学堂`)
     fs.writeFileSync(path.join(__dirname, 'wikitext-已替换', element), txt_content)
 });
